7178ccd660c0f177e00224b09b5f88100515d4ea
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-02-15 13:09:15 -0500

    adds RandomForestClassifier and transform() functions

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,9 +1,30 @@
 import numpy as np
 import pandas as pd
 
+from sklearn.ensemble import RandomForestClassifier as SklRandomForestClassifier
 from sklearn.linear_model import LinearRegression as SklLinearRegression
 from sklearn.preprocessing import MinMaxScaler as SklMinMaxScaler
 from sklearn.preprocessing import PolynomialFeatures as SklPolynomialFeatures
+
+
+class RandomForestClassifier(SklRandomForestClassifier):
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+
+  def fit(self, X, y, *args, **kwargs):
+    if not isinstance(X, pd.core.frame.DataFrame):
+      raise Exception("Feature input has wrong type. Please use pandas DataFrame")
+    if not (isinstance(y, pd.core.frame.DataFrame) or isinstance(y, pd.core.series.Series)):
+      raise Exception("Label input has wrong type. Please use pandas DataFrame or Series")
+
+    self.y_name = y.name if len(y.shape) == 1 else y.columns[0]
+    super().fit(X.values, y.values, *args, **kwargs)
+
+  def predict(self, X, *args, **kwargs):
+    if not isinstance(X, pd.core.frame.DataFrame):
+      raise Exception("Feature input has wrong type. Please use pandas DataFrame")
+    y_t = super().predict(X.values, *args, **kwargs)
+    return pd.DataFrame(y_t, columns=[self.y_name])
 
 
 class PolynomialFeatures(SklPolynomialFeatures):
@@ -17,6 +38,18 @@ class PolynomialFeatures(SklPolynomialFeatures):
     self.columns = X.columns
     self.shape = X.shape
     X_t = super().fit_transform(X.values, *args, **kwargs)
+    return pd.DataFrame(X_t, columns=self.get_feature_names_out())
+
+  def transform(self, X, *args, **kwargs):
+    if type(X) == np.ndarray:
+      return super().transform(X.values, *args, **kwargs)
+
+    if not isinstance(X, pd.core.frame.DataFrame):
+      raise Exception("Feature input has wrong type. Please use pandas DataFrame")
+    if list(self.columns) != list(X.columns) or self.shape[1] != X.shape[1]:
+      raise Exception("Input has wrong shape.")
+
+    X_t = super().transform(X.values, *args, **kwargs)
     return pd.DataFrame(X_t, columns=self.get_feature_names_out())
 
 
@@ -53,6 +86,18 @@ class MinMaxScaler(SklMinMaxScaler):
     X_t = super().fit_transform(X.values, *args, **kwargs)
     return pd.DataFrame(X_t, columns=X.columns)
 
+  def transform(self, X, *args, **kwargs):
+    if type(X) == np.ndarray:
+      return super().transform(X.values, *args, **kwargs)
+
+    if not isinstance(X, pd.core.frame.DataFrame):
+      raise Exception("Input has wrong type. Please use pandas DataFrame")
+    if list(self.columns) != list(X.columns) or self.shape[1] != X.shape[1]:
+      raise Exception("Input has wrong shape.")
+
+    X_t = super().transform(X.values, *args, **kwargs)
+    return pd.DataFrame(X_t, columns=X.columns)
+
   def inverse_transform(self, X, *args, **kwargs):
     if not (isinstance(X, pd.core.frame.DataFrame) or isinstance(X, pd.core.series.Series)):
       raise Exception("Input has wrong type. Please use pandas DataFrame or Series")
@@ -69,7 +114,7 @@ class MinMaxScaler(SklMinMaxScaler):
 
     if col != "":
       X = pd.DataFrame(X.values, columns=[col])
-      dummy_df = pd.DataFrame(np.zeros(self.shape), columns=self.columns)
+      dummy_df = pd.DataFrame(np.zeros((len(col_vals), self.shape[1])), columns=self.columns)
       dummy_df[col] = col_vals
       X_t = super().inverse_transform(dummy_df.values, *args, **kwargs)
       return pd.DataFrame(X_t, columns=self.columns)[[col]]
